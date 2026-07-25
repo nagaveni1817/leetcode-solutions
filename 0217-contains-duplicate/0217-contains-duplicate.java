4e941158e4783d866d0d0fa0ff22fd51bf35b9e1
Nagaveni <nagaveni79627@gmail.com>
2026-07-25 22:30:00 +0530

    Time: 19 ms (41.27%), Space: 111.9 MB (5.35%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer,Integer> map= new HashMap<>();
-        for(int i=0; i<nums.length; i++){
+        /*for(int i=0; i<nums.length; i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         for(int num : map.keySet()){
@@ -9,6 +9,14 @@ class Solution {
             if(freq>=2){
                 return true;
             }
+        }
+        return false;*/
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            map.put(nums[i],i);
         }
         return false;
     }
