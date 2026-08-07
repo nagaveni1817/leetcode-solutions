5f19d890cf773422bca65a6b35fa9cb356da12c4
Nagaveni <nagaveni79627@gmail.com>
2026-08-07 21:55:32 +0530

    Time: 0 ms (100%), Space: 42.6 MB (99.05%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,12 +3,13 @@ class Solution {
         String p=strs[0];
         for(int i=1; i<strs.length;i++){
             String str=strs[i];
-            int j=0;
-            while(j < p.length() && j < str.length() && p.charAt(j) == str.charAt(j))
+           // int j=0;
+            while(/*j < p.length() && j < str.length() && p.charAt(j) == str.charAt(j)*/ !str.startsWith(p) )
             {
-                j++;
+               // j++;
+               p=p.substring(0,p.length()-1);
            }
-           p=p.substring(0,j);
+           //p=p.substring(0,j);
         }
       return p;
     }
